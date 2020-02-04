46aaf5ec2aa8b74b48af94eba0173a794feb2051
radovan.barnabas <r.barnabas96@gmail.com>
2020-02-04 15:59:41 +0100

    test_build

---
+++ test_build/precache-manifest.ebe30bf41150fb7c3cd931994c461bff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d570a17430c7ca6db61c4e792c7311ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b57abbd3be268068f1c",
+    "url": "/static/css/main.ba16d839.chunk.css"
+  },
+  {
+    "revision": "7952ff67d16d344b43b9",
+    "url": "/static/js/2.ae1d5464.chunk.js"
+  },
+  {
+    "revision": "cc835925ffb0aaeea483c5e8a2900598",
+    "url": "/static/js/2.ae1d5464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b57abbd3be268068f1c",
+    "url": "/static/js/main.ffb2b88d.chunk.js"
+  },
+  {
+    "revision": "9ce52a4025b3f36f2337",
+    "url": "/static/js/runtime-main.47991398.js"
+  }
+]);
