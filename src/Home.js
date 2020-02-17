2ad4c9c8b2ecdd90b3486f042d1cedbbdf29b89f
radovan.barnabas <r.barnabas96@gmail.com>
2020-02-17 15:39:28 +0100

    loader added

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,9 @@ function Home(){
     const apiEndpoint = "https://public-recepy-api.herokuapp.com/recepts";
 
     const [recipes, setRecepies] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
 
-    useEffect( () => {
+    useEffect(() => {
         getRecepies();
     }, []);
 
@@ -15,11 +16,13 @@ function Home(){
         const response = await fetch(apiEndpoint);
         const data = await response.json();
         setRecepies(data);
+        document.getElementById('loader').classList.add('hidden');
     }
 
     return(
         <div>
             <h1>Welcome to the best Cake recipe website!</h1>
+            <div id="loader"></div>
 
             <div className="recipes">
                 {recipes.map(recipe => (
